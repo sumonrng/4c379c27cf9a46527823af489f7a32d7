d40e98a2e1e9dcfb68e6ef2f966759d99bdb5ecc
sumonrng <sumonmti498@gmail.com>
2022-09-18 05:12:15 +0600

    first_trac_commit

---
+++ httpdocs/index.php
@@ -0,0 +1,322 @@
+<?php include "part/header.php"; ?>
+
+<section class="home-banner" id="banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 position-u flex-align wow fadeInLeft">
+                <div class="banner-contain">
+                    <h1 class="banner-heading">Invest In <span>Robo CFX</span> Way To Trade</h1>
+                    <p class="banner-des">We build a solid & reliable architecture for the community who want to robotic
+                        auto trade in Crypto & Forex without any previous experience & less risk of lose their purchase.
+                    </p>
+                    <a href="#" class="btn">Learn more</a>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 position-u wow fadeInRight">
+                <div class="banner-img">
+                    <img src="images/about-2.png" alt="banner">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="work-part lightskyblue ptb-100">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 wow fadeInUp">
+                <div class="section-heading text-center pb-65">
+                    <label class="sub-heading">what is Robotic auto trading</label>
+                    <h2 class="heading-title">How it Works</h2>
+                    <p class="heading-des">Robotic auto trading is the process of using a computer program that follows
+                        a defined set of instructions for placing a trade order.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 text-center flex-align justify-center wow fadeInLeft">
+                <div class="work-box">
+                    <div class="work-box-bg"></div>
+                    <img src="images/work-process.png" class="rotation-img" alt="Work Process">
+                </div>
+            </div>
+            <div class="col-md-6 flex-align wow fadeInRight">
+                <div class="work-box">
+                    <h3 class="work-process-title pb-25">We've built a algo trading platform.</h3>
+                    <p class="work-des pb-20">The aim of the algorithmic trading program is to dynamically identify
+                        profitable opportunities and place the trades in order to generate profits at a speed and
+                        frequency that is impossible to match by a human trader. Given the advantages of higher accuracy
+                        and lightning-fast execution speed, trading activities based on computer algorithms have gained
+                        tremendous popularity.</p>
+
+                    <ul class="check-list">
+                        <li><span><i class="fa fa-check" aria-hidden="true"></i></span>
+                            <p>Algorithmic investing has become a viable and profitable strategy for any investor to
+                                implement in 2022.</p>
+                        </li>
+                        <li><span><i class="fa fa-check" aria-hidden="true"></i></span>
+                            <p>Beginners can use algo trading to successfully carry out trading activities without any
+                                hassles and long trading hours.</p>
+                        </li>
+                        <li><span><i class="fa fa-check" aria-hidden="true"></i></span>
+                            <p>According to Equedia, algorithmic trading systems can process an order in 10 milliseconds
+                                or less. For comparison, it takes the human eye around 300 milliseconds to blink.</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="feature-part pt-100 pb-10" id="Features">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 wow fadeInUp">
+                <div class="section-heading text-center pb-65">
+                    <label class="sub-heading">Robo CFX Feature</label>
+                    <h2 class="heading-title">Best Features</h2>
+                    <p class="heading-des">Robo CFX is a Forex Based futures Trading with its own native Algorithmic
+                        Trading.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 wow fadeInUp pb-80">
+                <div class="feature-box">
+                    <div class="feature-icon">
+                        <img src="images/feature-1.png" alt="Safe & Secure">
+                    </div>
+                    <div class="feature-contain pt-25">
+                        <a href="feature.html" class="feature-title">Safe & Secure</a>
+                        <p class="feature-des">Secured investment accounts or securities are real or virtual documents
+                            that represent any of the following trade-able assets: ownership of publicly-traded shares
+                            of a stock corporation.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 wow fadeInUp pb-80">
+                <div class="feature-box">
+                    <div class="feature-icon">
+                        <a href="feature.html"><img src="images/feature-2.png" alt="Early Bonus"></a>
+                    </div>
+                    <div class="feature-contain pt-25">
+                        <a href="feature.html" class="feature-title">Monthly Bonus</a>
+                        <p class="feature-des">Profits are distributed in the first week of each month. The company that
+                            makes a profit from trading gets 60% of the customers directly. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 wow fadeInUp pb-80">
+                <div class="feature-box">
+                    <div class="feature-icon">
+                        <a href="feature.html"><img src="images/feature-3.png" alt="Univarsal Access"></a>
+                    </div>
+                    <div class="feature-contain pt-25">
+                        <a href="feature.html" class="feature-title">Affiliate Facility</a>
+                        <p class="feature-des">There are lots of income opportunities through secure trading as well as
+                            affiliate marketing.E.g. Customer Refferral Bonus, Generation of team income, Spread
+                            commission of team invest,Globlal Pool etc. </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="timeline lightskyblue ptb-100" id="TimeLine">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 wow fadeInUp">
+                <div class="section-heading text-center pb-65">
+                    <label class="sub-heading">roadmap</label>
+                    <h2 class="heading-title">The Timeline</h2>
+                    <p class="heading-des">Our Historic Initiative Towards Goal.</p>
+                </div>
+            </div>
+        </div>
+        <div class="main-roadmap">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="h-border wow fadeInLeft"></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="roadmap-slider owl-carousel">
+                        <div class="roadmap wow fadeInLeft">
+                            <div class="roadmap-box text-center">
+                                <div class="date-title">October 2021</div>
+                                <div class="map-graphic">
+                                    <div class="small-round"><span></span></div>
+                                    <div class="v-row"></div>
+                                </div>
+                                <div class="roadmap-detail-box">
+                                    <p>Concept Development : idea of algorithmic trading architecture Developed. </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="roadmap wow fadeInLeft">
+                            <div class="roadmap-box text-center">
+                                <div class="date-title">November 2021</div>
+                                <div class="map-graphic">
+                                    <div class="small-round"><span></span></div>
+                                    <div class="v-row"></div>
+                                </div>
+                                <div class="roadmap-detail-box">
+                                    <p>We launch our algo trading deposit plan globally.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="roadmap wow fadeInLeft">
+                            <div class="roadmap-box text-center">
+                                <div class="date-title">January 2023</div>
+                                <div class="map-graphic">
+                                    <div class="small-round"><span></span></div>
+                                    <div class="v-row"></div>
+                                </div>
+                                <div class="roadmap-detail-box">
+                                    <p>Arrangements are made to get Global Pool Commission to at least hundred traders.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="roadmap wow fadeInLeft">
+                            <div class="roadmap-box text-center">
+                                <div class="date-title">January 2024</div>
+                                <div class="map-graphic">
+                                    <div class="small-round"><span></span></div>
+                                    <div class="v-row"></div>
+                                </div>
+                                <div class="roadmap-detail-box">
+                                    <p>We launch our own crypto currency. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="team-part pt-100 pb-55" id="Team">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 wow fadeInUp">
+                <div class="section-heading text-center pb-65">
+                    <label class="sub-heading">meet the team</label>
+                    <h2 class="heading-title">Our Team</h2>
+                    <p class="heading-des">Expert who are working being the idea of this exchange to give best to the
+                        users.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 wow fadeInLeft pb-45">
+                <div class="team-box flex-align">
+                    <div class="team-img">
+                        <a href="team.html"><img src="images/team-1.jpg" alt="team member"></a>
+                    </div>
+                    <div class="team-des">
+                        <a href="team.html" class="member-name">DOROTHY P. SHAW</a>
+                        <p class="member-des">Founder</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 pb-45 wow fadeInRight">
+                <div class="team-box flex-align">
+                    <div class="team-img">
+                        <a href="team.html"><img src="images/team-2.jpg" alt="team member"></a>
+                    </div>
+                    <div class="team-des">
+                        <a href="team.html" class="member-name">RODNEY ROSE</a>
+                        <p class="member-des">CEO</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 wow fadeInLeft pb-45">
+                <div class="team-box flex-align">
+                    <div class="team-img">
+                        <a href="team.html"><img src="images/team-3.jpg" alt="team member"></a>
+                    </div>
+                    <div class="team-des">
+                        <a href="team.html" class="member-name">RASMUS NIELSEN</a>
+                        <p class="member-des">Consultant</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 pb-45 wow fadeInRight">
+                <div class="team-box flex-align">
+                    <div class="team-img">
+                        <a href="team.html"><img src="images/team-4.jpg" alt="team member"></a>
+                    </div>
+                    <div class="team-des">
+                        <a href="team.html" class="member-name">CHANDNI CORTES</a>
+                        <p class="member-des">Consultant</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="faq-part pt-100">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 wow fadeInUp">
+                <div class="section-heading text-center pb-65">
+                    <label class="sub-heading">Faqs</label>
+                    <h2 class="heading-title">Frequently Asked questions</h2>
+                    <p class="heading-des">Expert who are working to clear your concept share some questions and
+                        answers.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 wow fadeInUp">
+                <div class="tab-content">
+                    <div class="tab-panel active" id="tab-1">
+                        <div class="row">
+                            <div class="col-md-6 pb-65">
+                                <div class="faq-tab">
+                                    <a href="faq.html" class="qus-title">What is Trading?</a>
+                                    <p class="qus-des pt-10">Trading is very simple concept. You will be benefited by
+                                        your knowledge & experience from buy & sell something.</p>
+                                </div>
+                            </div>
+                            <div class="col-md-6 pb-65">
+                                <div class="faq-tab">
+                                    <a href="faq.html" class="qus-title">What is Algorithmic Trading? </a>
+                                    <p class="qus-des pt-10">The aim of the algorithmic trading program is to
+                                        dynamically identify profitable opportunities and place the trades in order to
+                                        generate profits at a speed and frequency that is impossible to match by a human
+                                        trader. </p>
+                                </div>
+                            </div>
+                            <div class="col-md-6 pb-65">
+                                <div class="faq-tab">
+                                    <a href="faq.html" class="qus-title">What is Trade With Robotic Technology?</a>
+                                    <p class="qus-des pt-10">Robotic Technology (Artificial Intelligent) is the
+                                        algorithm who will always monitor data from various trading resources and try to
+                                        decide right time to sale and buy continuously.</p>
+                                </div>
+                            </div>
+                            <div class="col-md-6 pb-65">
+                                <div class="faq-tab">
+                                    <a href="faq.html" class="qus-title">How can I withdraw the deposit amount?</a>
+                                    <p class="qus-des pt-10">Robo CFX is a secure trading platform. Here the security of
+                                        everyone's deposit is ensured. Withdrawals are subject to one month's notice.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include "part/footer.php"; ?>
